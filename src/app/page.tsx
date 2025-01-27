b5c89a057722d046128a09846e0fc7721b2c78bb
Darshan M U <mu_darshan@yahoo.com>
2025-01-27 17:58:12 +0530

    Show images to only signed-in users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
-const images = await db.query.images.findMany({
-  orderBy: (model, {desc}) => desc(model.id),
-});
+async function Images() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id),
+  });
 
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap gap-4">
         {images.map((img) => (
           <div key={img.id} className="w-48 h-32 flex flex-col">
             <img src={img.url} alt="image" className="h-full w-full object-cover"/>
@@ -18,6 +17,18 @@ const images = await db.query.images.findMany({
           </div>
         ))}
       </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign In Above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
