6524c02f3ba45ae0fc945e52a0db6f892a251f8a
Darshan M U <mu_darshan@yahoo.com>
2025-01-28 21:35:41 +0530

    Fix upload button and add shadcn toaster

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,25 +1,17 @@
-"use client";
-
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export function TopNav() {
-    const router = useRouter();
   return (
     <nav className="flex justify-between items-center w-full p-4 text-xl font-semibold border-b p-4 mb-6">
       <div> Test App Gallery</div>
-      <div className="flex flex-row">
+      <div className="flex flex-row gap-4 items-center">
         <SignedOut>
             <SignInButton />
         </SignedOut>
         <SignedIn>
-        <UploadButton 
-            endpoint="imageUploader"
-            onClientUploadComplete={(metadata) => {
-                router.refresh();
-            }}    
-        />
+        <SimpleUploadButton />  
         <UserButton />
         </SignedIn>
       </div>
